e8c9e71bb26801ed027319e66de7be6c597a5fa8
anaandreis <anapauladeandreis@gmail.com>
2023-02-16 11:50:36 -0300

    THE CODE FOR THE DICE

--- app/src/main/java/com/anaandreis/diceroll/MainActivity.kt
+++ app/src/main/java/com/anaandreis/diceroll/MainActivity.kt
@@ -3,6 +3,7 @@ package com.anaandreis.diceroll
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,39 +15,21 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countButton: Button = findViewById(R.id.countUp_button)
-        countButton.setOnClickListener { countUp() }
-
-        val resetButton: Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice() {
        // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
-    }
-
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else {
-            var resultInt = resultText.text.toString().toInt()
-
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-    }
-
-    private fun reset() {
-        val resultText: TextView =  findViewById(R.id.result_text)
-        var resultInt = resultText.text.toString().toInt()
-        resultInt = 0
-        resultText.text = resultInt.toString()
+        diceImage.setImageResource(drawableResource)
     }
 }
